8db4d30521d0d42f5aa7bf1a2e2d7c131ed842e2
Yonarp <pranoy0211@gmail.com>
2020-11-05 20:45:23 +0530

    implemented styling on menu-items

--- src/Components/menu-item/menu-item.component.jsx
+++ src/Components/menu-item/menu-item.component.jsx
@@ -3,14 +3,18 @@ import './menu-item.styles.scss';
 
 const MenuItem = ({title, imageUrl ,key,size}) => (
 
-    <div style = {{
-        backgroundImage: `url(${imageUrl})`,
-    }} 
-    className= {`${size} menu-item`}>
-        <div className="content">
-            <h1 className='title'>{title}</h1>
-            <span className='subtitle'>Shop Now</span>
-        </div>
+    <div className= {`${size} menu-item`}>
+        <div className="background" style = {{
+            background: `linear-gradient(to top,rgba(0,0,0,0.7),rgba(0,0,0,0.4)) , url(${imageUrl}) `,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize:'cover',
+            backgroundPosition: 'center'
+                }} ></div>
+                <div className="content">
+                    <h1 className='title'>{title}</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+        
     </div>
 );
 
